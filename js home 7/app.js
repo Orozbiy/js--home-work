688914b5dbe9b31c51fa99c44b3500f0c86f6b26
unknown <orozbijhodzebekov@gmail.com>
2026-03-11 13:29:28 +0600

    Балл

--- js home 7/app.js
+++ js home 7/app.js
@@ -1,17 +1,50 @@
-let tovar = prompt('	Дисциплина');
-let pro = Number(prompt('РК1'));
-let prooo = Number(prompt('Экзамен'));
+let name = prompt("Атыңыз ким?");
 
-let tal = pro + prooo; 
 
-alert('Дисциплина' + tovar);
+let subject1 = prompt("1-сабактын аталышы:");
+let subject2 = prompt("2-сабактын аталышы:");
+let subject3 = prompt("3-сабактын аталышы:");
 
-if (tal > 50) {
-    
-    let fSum = (tal +60);
-    alert('общие : ' + fSum + ' сом');
+
+let opozdanie = Number(prompt("Жалпы опозданиелер саны:"));
+
+
+if (opozdanie > 10) {
+    alert("Урматтуу " + name + ", сиз опоздание 10дон ашып кеткендиктен экзаменден куладыңыз!");
 } else {
-    
-    alert('сред баллы ' + tal + ' сом');
+   
+    let core1 = Number(prompt(subject1 + "  алган балыңыз (0-95)"));
+    let core2 = Number(prompt(subject2 + "  алган балыңыз (0-90)"));
+    let core3 = Number(prompt(subject3 + "  алган балыңыз (0-95)"));
+
+   
+    let averageScore = (core1 + core2 + core3) / 3;
+
+   
+    if (opozdanie === 0){
+        averageScore += 5;
+        alert("Сонун! Сизде опоздание жок, орточо балыңызга 5 балл кошулду.");
+    }
+
+    if (averageScore > 90) {
+        averageScore = 90;
+    }
+
+  
+    let resultMessage = "к";
+    if (averageScore >= 90) {
+        resultMessage = "Эң жакшы (5)";
+    } else if (averageScore >= 70) {
+        resultMessage = "Жакшы (4)";
+    } else if (averageScore >= 50) {
+        resultMessage = "Удов ";
+    } else {
+        resultMessage = "Кайрадан тапшыр отподун ";
+    }
+
+  
+    alert("Студент: " + name + "\n" +
+          "Орточо балл: " + averageScore.toFixed(1) + "\n" +
+          "Жыйынтык: " + resultMessage);
 }
 
---
+++ js home 9/app.js
@@ -0,0 +1,29 @@
+
+ 
+    let name = prompt("Студенттин атын киргизиңиз:");
+
+
+    let mathem = Number(prompt("Математика боюнча баа (0-100):"));
+    let lang = Number(prompt("Тилдер боюнча баа (0-100):"));
+    let program = Number(prompt("Программалоо боюнча баа (0-100):"));
+    
+
+    let absences = Number(prompt("Калтырылган сабактардын саны:"));
+
+
+    let avera = (mathem + lang + program) / 3;
+
+
+    if (absences === 0) {
+        avera += 5;
+        alert("Бонус +5 балл идеалдуу катышуу үчүн!");
+    }
+
+   
+    if (avera >= 90) {
+        alert("Куттуктайбыз, " + name + "! Жогорулатылган стипендия. Сиздин балл: " + avera.toFixed(1));
+    } else if (avera >= 70) {
+        alert("Кадимки стипендия берилди. Сиздин балл: " + avera.toFixed(1));
+    } else {
+        alert("Студент " + name + ", сиз сессиядан өтпөй калдыңыз. Орточо балл: " + avera.toFixed(1));
+    }

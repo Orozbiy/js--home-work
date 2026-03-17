52b8be70b4377ad1d5440b76a438ea61fdea49e1
unknown <orozbijhodzebekov@gmail.com>
2026-03-17 17:35:34 +0600

    first commit

--- js home 11/app.js
+++ js home 11/app.js
@@ -1,28 +1,22 @@
 
-let balance = prompt("Балансыңызды жазыңыз ");
-balance = Number(balance); 
-
-let choice = prompt("Аракетти тандаңыз: 1 - Валютага алмаштыруу, 2 - Сатып алууну текшерүү");
-
-if (choice === "1") {
-   
+function convertCurrency(balance) {
     let currency = prompt("Кайсы валютага (USD же EUR)").toUpperCase();
     let rate;
 
     if (currency === "USD") {
-        rate = 87; 
+        rate = 87.5;
         alert("Сиздин баланс USD менен: " + (balance / rate).toFixed(2));
     } else if (currency === "EUR") {
-        rate = 9; 
+        rate = 100; 
         alert("Сиздин баланс EUR менен: " + (balance / rate).toFixed(2));
     } else {
         alert("Мындай валюта жок");
     }
+}
 
-} else if (choice === "2") {
-    
-    let price = prompt("Товардын баасы канча?");
-    price = Number(price);
+
+function checkPurchase(balance) {
+    let price = Number(prompt("Товардын баасы канча?"));
 
     if (balance >= price) {
         let remainder = balance - price;
@@ -31,7 +25,32 @@ if (choice === "1") {
         let missing = price - balance;
         alert("Тилекке каршы, акча жетпейт. Дагы " + missing + " сом керек.");
     }
-
-} else {
-    alert("Туура эмес киргизилген ");
 }
+
+
+
+
+
+function startApp() {
+    let userInput = prompt("Балансыңызды жазыңыз ");
+    let balance = Number(userInput);
+
+
+    if (isNaN(balance)) {
+        alert("Сураныч, сан киргизиңиз!");
+        return;
+    }
+
+    let choice = prompt("Аракетти тандаңыз: 1 - Валютага алмаштыруу, 2 - Сатып алууну текшерүү");
+
+    if (choice === "1") {
+        convertCurrency(balance);
+    } else if (choice === "2") {
+        checkPurchase(balance);
+    } else {
+        alert("Туура эмес тандоо киргизилди");
+    }
+}
+
+
+startApp();
